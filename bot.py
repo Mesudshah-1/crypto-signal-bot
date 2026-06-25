9837d7b88c0a96ceb756f8715aeecf1c031fb5ef
Mesudshah-1 <mesudshah@protonmail.com>
2026-06-25 14:48:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
 import os
 import requests
 
-TOKEN = os.environ["8703847181:AAGrOqw8hWrQqmIIpe7JYYqZrz81QMGzbe0"]
-CHAT_ID = os.environ["-1003953455562"]
+print("BOT STARTED")
 
-btc = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT").json()["price"]
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("GENERAL_CHANNEL_ID")
 
-requests.post(
+print("TOKEN OK:", bool(TOKEN))
+print("CHAT ID:", CHAT_ID)
+
+btc = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT").json()
+print("BINANCE OK:", btc)
+
+message = f"BTC: {btc['price']}"
+
+r = requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     json={
         "chat_id": CHAT_ID,
-        "text": f"📊 BTC: {btc}"
+        "text": message
     }
 )
+
+print("TELEGRAM RESPONSE:", r.text)
