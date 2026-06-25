98ca5d6f248d39909430b3a7085fc44f7cfe797c
Mesudshah-1 <mesudshah@protonmail.com>
2026-06-25 12:54:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-print("HELLO FROM GITHUB ACTIONS")
+import requests
+
+TOKEN = "8703847181:AAG7ZoIJ4XHpqniqm2wp16ZUCHJL7tIzctg"
+CHAT_ID = "-1003953455562"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+r = requests.post(url, json={
+    "chat_id": CHAT_ID,
+    "text": "TEST MESAJI - GELİYOR MU?"
+})
+
+print(r.status_code)
+print(r.text)
