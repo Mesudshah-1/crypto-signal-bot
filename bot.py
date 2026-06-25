c543b22afc221ec71477c920890fe0317083d92e
Mesudshah-1 <mesudshah@protonmail.com>
2026-06-25 14:55:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,23 @@
 import requests
 
+print("BOT STARTED")
+
 TOKEN = "8703847181:AAGrOqw8hWrQqmIIpe7JYYqZrz81QMGzbe0"
 CHAT_ID = "-1003953455562"
 
-btc = requests.get(
-    "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-).json()["price"]
+print("STEP 1 OK")
 
-text = f"📊 BTC: {btc}"
+btc = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+
+print("BINANCE STATUS:", btc.status_code)
+print("BINANCE TEXT:", btc.text)
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 r = requests.post(url, json={
     "chat_id": CHAT_ID,
-    "text": text
+    "text": "FINAL TEST"
 })
 
-print(r.text)
+print("TELEGRAM STATUS:", r.status_code)
+print("TELEGRAM RESPONSE:", r.text)
